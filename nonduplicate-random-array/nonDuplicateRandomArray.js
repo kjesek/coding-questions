0bb59e24cfcaeac9d4561b355dfcf516ab425888
kjesek <krista.adelle@gmail.com>
2018-11-15 22:31:25 -0600

    Add parameters to create random array function so that it can be used for any random range and array length

--- nonduplicate-random-array/nonDuplicateRandomArray.js
+++ nonduplicate-random-array/nonDuplicateRandomArray.js
@@ -1,25 +1,24 @@
-function createRandomArray(){
-	let randomIntArray = [getRandomNum(1, 11)]; 
-	while(!checkArrayLength(randomIntArray, 4)){
-        nextNum = getRandomNum(1, 11);
-        if(checkIfInArray(nextNum, randomIntArray)){
-            randomIntArray.push(nextNum);
-        } 
+function createRandomArray(minNum, maxNum, arrayLength) {
+  let randomIntArray = [getRandomNum(minNum, maxNum)];
+  while (!checkArrayLength(randomIntArray, arrayLength)) {
+    nextNum = getRandomNum(minNum, maxNum);
+    if (checkIfInArray(nextNum, randomIntArray)) {
+      randomIntArray.push(nextNum);
     }
-    return randomIntArray; 
+  }
+  return randomIntArray;
 }
 
-function checkArrayLength(array, expectedLength){
-	return array.length === expectedLength;
+function checkArrayLength(array, expectedLength) {
+  return array.length === expectedLength;
 }
 
-function checkIfInArray(num, array){
-	return array.indexOf(num) === -1;
+function checkIfInArray(num, array) {
+  return array.indexOf(num) === -1;
 }
 
-function getRandomNum(min, max){ 
-    return  Math.floor(Math.random() * (max - min)) + min;
+function getRandomNum(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
 }
 
-console.log(createRandomArray())
-
+console.log(createRandomArray(1, 11, 4));
