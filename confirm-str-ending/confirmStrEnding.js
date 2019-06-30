b2a6e416df3bb165321f1d47617ebd46a195da18
kjesek <krista.adelle@gmail.com>
2019-06-30 15:11:06 -0500

    removes comments for confirm string ending

--- confirm-str-ending/confirmStrEnding.js
+++ confirm-str-ending/confirmStrEnding.js
@@ -1,17 +1,4 @@
-/* function confirmEnding
-* @param string
-* @param string
-* return boolean
-*/
-const confirmEnding = (str, target) => {
-  // target string to be the end of str 
-  // substring can return the end of the string with a number for the index
-  //  str length minus the  target length
-  return str.substring(str.length - target.length) === target;
-  // return the substring of above
-  // check if the substring above is equal to the target, return true or false
+const confirmEnding = (str, target) => { 
+  return str.substring(str.length - target.length) === target; 
 }
 
-// hello, lo 
-
-console.log(confirmEnding('hello', 'h'));
